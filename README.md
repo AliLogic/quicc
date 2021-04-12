2c6be24adf18f7fa068d936efc35eaa54244f6ca
AliLogic <drfiniz@gmail.com>
2021-04-12 22:45:47 +0500

    Add: Redirect deletion and cleanup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # quicc
-Quicc, is a GoLang based link shortener based on Maps and JSON
+Quicc, is a GoLang based link shortener based on Maps and JSON.
+
+## Get Started!
+You need to make an .env file with QUICC_FILE pointing to the path where the JSON file will be located.
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var mapVar = make(map[string]string)
+var urlMap = make(map[string]string)
 var quiccFile string
 
 func main() {
@@ -22,7 +22,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// check if file exist or not first
+	// check if file exists or not
 	if quiccFile = os.Getenv("QUICC_FILE"); len(quiccFile) == 0 {
 		log.Fatal("Environment variable QUICC_FILE is empty!")
 	}
@@ -45,20 +45,21 @@ func main() {
 	}
 
 	// if it does, then continue to read the file
-	data, err := os.ReadFile(quiccFile)
-	if err != nil {
+	var data []byte
+	if data, err = os.ReadFile(quiccFile); err != nil {
 		// file does exist but something is wrong, don't continue
 		// fmt.Println("something went wrong while reading the file")
 		log.Fatal(err)
 	}
 
-	if err = json.Unmarshal(data, &mapVar); err != nil {
+	if err = json.Unmarshal(data, &urlMap); err != nil {
 		// failed to parse json to map, don't continue
 		log.Fatal(err)
 	}
 
 	http.HandleFunc("/", redirectHandler)
 	http.HandleFunc("/add/", additionHandler)
+	http.HandleFunc("/delete/", deletionHandler)
 
 	// it's important that we check our http server is alive or not
 	if err = http.ListenAndServe(":8080", nil); err != nil {
@@ -66,15 +67,38 @@ func main() {
 	}
 }
 
-func additionHandler(w http.ResponseWriter, r *http.Request) {
-	// here, we get all the post method stuff
-	// let's process UwU
-
-	// guard clauses (early return) go brrr
+func deletionHandler(w http.ResponseWriter, r *http.Request) {
+	// Only accept POST method
 	if r.Method != "POST" {
 		return
 	}
 
+	// failed to parse form? panic!
+	if err := r.ParseForm(); err != nil {
+		log.Panic(err)
+	}
+
+	key := r.FormValue("key")
+
+	// key validation
+	if _, exists := getKey(key); !exists {
+		fmt.Fprintf(w, "Key '%s' is not registered!", key)
+		return
+	}
+
+	// delete key
+	delete(urlMap, key)
+	fmt.Fprintf(w, "Key '%s' is deleted!", key)
+	saveLinks()
+}
+
+func additionHandler(w http.ResponseWriter, r *http.Request) {
+	// Only accept POST method
+	if r.Method != "POST" {
+		return
+	}
+
+	// failed to parse form? panic1
 	if err := r.ParseForm(); err != nil {
 		log.Panic(err)
 	}
@@ -82,32 +106,33 @@ func additionHandler(w http.ResponseWriter, r *http.Request) {
 	key := r.FormValue("key")
 	link := r.FormValue("link")
 
-	_, ok := mapVar[key]
-	if ok {
+	// key validation
+	if _, exists := getKey(key); exists {
 		fmt.Fprintf(w, "Key '%s' already exists!", key)
-	} else {
-		mapVar[key] = link
-		fmt.Fprintf(w, "Key '%s' is now registered to redirect to '%s'", key, link)
-		saveLinks()
+		return
 	}
+
+	// key registered!
+	urlMap[key] = link
+	fmt.Fprintf(w, "Key '%s' is now registered to redirect to '%s'", key, link)
+	saveLinks()
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	// okay, so we have received some path in our link,
-	// let's check if it is valid or not (by checking in our map)
-
+	// User didn't ask for a redirect? Serve them the homepage
 	key := r.URL.Path[1:]
 	if len(key) == 0 {
 		http.ServeFile(w, r, "index.html")
 		return
 	}
 
-	link, ok := mapVar[key]
-	if ok {
+	// key validation before we redirect
+	if link, exists := getKey(key); exists {
 		http.Redirect(w, r, link, http.StatusFound)
-	} else {
-		fmt.Fprintf(w, "'%s' is not a valid redirect key!", key)
+		return
 	}
+
+	fmt.Fprintf(w, "'%s' is not a valid redirect key!", key)
 }
 
 func saveLinks() {
@@ -115,13 +140,17 @@ func saveLinks() {
 	var data []byte
 	var err error
 
-	if data, err = json.Marshal(mapVar); err != nil {
+	if data, err = json.Marshal(urlMap); err != nil {
 		log.Fatal(err)
 	}
 
-	err = ioutil.WriteFile(quiccFile, data, 0)
-	if err != nil {
+	if err = ioutil.WriteFile(quiccFile, data, 0); err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+}
+
+func getKey(key string) (string, bool) {
+	link, exists := urlMap[key]
+	return link, exists
 }
